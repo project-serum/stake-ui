637da6ae69bedbb85cd5d7d8925c6e1f3f791406
Armani Ferrante <armaniferrante@gmail.com>
2021-08-18 19:34:30 -0700

    Wallets (#4)

--- src/components/common/OwnedTokenAccountsSelect.tsx
+++ src/components/common/OwnedTokenAccountsSelect.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import BN from 'bn.js';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import { PublicKey } from '@solana/web3.js';
-import { State as StoreState } from '../../store/reducer';
-import { toDisplay } from '../../utils/tokens';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import BN from "bn.js";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import { PublicKey } from "@solana/web3.js";
+import { State as StoreState } from "../../store/reducer";
+import { toDisplay } from "../../utils/tokens";
 
 type Props = {
   style?: any;
   mint?: PublicKey | null;
   decimals?: number;
-  variant?: 'outlined' | 'standard';
+  variant?: "outlined" | "standard";
   onChange: (from: PublicKey, maxAmount: BN) => void;
 };
 
@@ -21,13 +21,12 @@ export default function OwnedTokenAccountsSelect(p: Props) {
     if (!mint) {
       return [];
     }
-		console.log('state', state.common);
     return state.common.ownedTokenAccounts.filter(
-      ota => ota.account.mint.toString() === mint.toString(),
+      (ota) => ota.account.mint.toString() === mint.toString()
     );
   });
 
-  const [fromAccount, setFromAccount] = useState('');
+  const [fromAccount, setFromAccount] = useState("");
 
   return (
     <Select
@@ -35,35 +34,35 @@ export default function OwnedTokenAccountsSelect(p: Props) {
       variant={variant}
       fullWidth
       value={fromAccount}
-      onChange={e => {
+      onChange={(e) => {
         const pk = e.target.value as string;
         setFromAccount(pk);
         const pubkey = new PublicKey(pk);
         const token = ownedTokenAccounts
-          .filter(ota => ota.publicKey.equals(pubkey))
+          .filter((ota) => ota.publicKey.equals(pubkey))
           .pop();
         onChange(pubkey, new BN(token!.account.amount));
       }}
     >
       {ownedTokenAccounts.length === 0 ? (
-        <MenuItem value={''}>No token accounts found</MenuItem>
+        <MenuItem value={""}>No token accounts found</MenuItem>
       ) : (
-        ownedTokenAccounts.map(ownedTokenAccount => {
+        ownedTokenAccounts.map((ownedTokenAccount) => {
           return (
             <MenuItem value={ownedTokenAccount.publicKey.toString()}>
               <div
                 style={{
-                  width: '100%',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  overflow: 'hidden',
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  overflow: "hidden",
                 }}
               >
                 <div>{`${ownedTokenAccount.publicKey}`}</div>
                 {decimals && (
-                  <div style={{ float: 'right', color: '#ccc' }}>{`${toDisplay(
+                  <div style={{ float: "right", color: "#ccc" }}>{`${toDisplay(
                     ownedTokenAccount.account.amount,
-                    decimals ?? 0,
+                    decimals ?? 0
                   )}`}</div>
                 )}
               </div>
